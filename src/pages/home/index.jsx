d6d77cba4774e425513b49d0a3295e401705cd04
fikriwado <fixwad.online@gmail.com>
2024-04-19 16:42:10 +0700

    update loading

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -27,15 +27,15 @@ const Home = () => {
       </section>
 
       <section className='py-10'>
-        <div className='container mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8'>
-          {products.length > 0 ? (
-            products.map((product) => (
+        {products.length > 0 ? (
+          <div className='container mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8'>
+            {products.map((product) => (
               <ProductCard key={product.id} product={product} />
-            ))
-          ) : (
-            <div className='text-center'>Loading...</div>
-          )}
-        </div>
+            ))}
+          </div>
+        ) : (
+          <div className='text-center'>Loading...</div>
+        )}
       </section>
     </Layout>
   )
